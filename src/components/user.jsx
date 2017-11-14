6260d932bc35dc62b9628333baf153bc62e7f880
16Q45T <etancelin.edouard@gmail.com>
2017-11-14 22:02:08 +0100

    Refacto the user component to clearly separate logic when the component is edit and when not

--- src/components/user.jsx
+++ src/components/user.jsx
@@ -36,46 +36,82 @@ class User extends React.Component {
     this.setState({isHuman})
   }
 
+
   render () {
     const { name, description, isHuman, img, isEditing, imgBatch } = this.state
     const { onNameInputChange, onDescriptionInputChange, onImgInputChange,toggleIsHuman } = this
-    return (
+    // Decide to clearly separate the Logic when the component is edit with an if statement.
+    // As logic is very different and it's make the code more readable
+    // Drawback ui is design twice
+      if (isEditing) {
+        return (
       <div className={`user-card ${isEditing ? "is-editing": ""} `}
         onClick={this.getClickHandler(() => this.setState({isEditing:true}))}
       >
+        <img
+          className="user-card-img img-is-editing"
+          src={img}>
+        </img>
+        {isEditing ?
+          <input
+            className="img-url-input"
+            value={imgBatch}
+            onChange={onImgInputChange}
+          ></input>:""}
+        <div className="user-card-content" >
+
+          <input
+            className="user-card-name name-is-editing"
+            value={name}
+            onChange={onNameInputChange}
+          ></input>
+
+          <input
+            className="user-card-description description-is-editing"
+            value={description}
+            onChange={onDescriptionInputChange}
+            ></input>
+
+          <div
+            className="user-card-is-human is-human-is-editing"
+            onClick={toggleIsHuman}
+          >
+            {isHuman? "✓": "" }
+          </div>
+        </div>
+      </div>)
+    } else {
+      return(
+        <div className="user-card"
+            onClick={this.getClickHandler(() => this.setState({isEditing:true}))}
+        >
           <img
-            className={`user-card-img ${isEditing ? "img-is-editing": ""} `}
+            className="user-card-img"
             src={img}>
           </img>
-          {isEditing ?
-            <input
-              className="img-url-input"
-              value={imgBatch}
-              onChange={onImgInputChange}
-            ></input>:""}
           <div className="user-card-content" >
 
             <input
-              className={`user-card-name ${isEditing ? "name-is-editing": ""} `}
+              className="user-card-name"
               value={name}
               onChange={onNameInputChange}
             ></input>
 
             <input
-              className={`user-card-description ${isEditing ? "description-is-editing": ""} `}
+              className="user-card-description"
               value={description}
               onChange={onDescriptionInputChange}
               ></input>
 
             <div
-              className={`user-card-is-human ${isEditing ? "is-human-is-editing": ""} `}
-              onClick={toggleIsHuman}
+              className="user-card-is-human"
             >
               {isHuman? "✓": "" }
             </div>
           </div>
-      </div>
-    );
+        </div>
+      )
+    }
   }
 }
 
